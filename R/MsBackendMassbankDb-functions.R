a7054766bd37531da78ed99eb4a049c477cc5a49
jorainer <johannes.rainer@gmail.com>
2020-10-16 19:19:39 +0200

    feat: implement functionality to get data from database

--- R/MsBackendMassbankDb-functions.R
+++ R/MsBackendMassbankDb-functions.R
@@ -26,36 +26,93 @@ MsBackendMassbankDb <- function() {
 #' The data can be either:
 #' - in the database.
 #' - in the local data (if new variables were added with $name <-).
+#' - core spectra variables - if they are not in the database they have to be
+#'   initialized with `NA` and the correct data type.
 #'
 #' @return a `data.frame` - always, even if only with a single column.
+#'
+#' @importFrom S4Vectors NumericList
 #'
 #' @author Johannes Rainer
 #'
 #' @noRd
-.spectra_data_massbank <- function(x, columns = spectraVariables(x)) {
+.spectra_data_massbank_sql <- function(x, columns = spectraVariables(x)) {
     db_cols <- intersect(x@spectraVariables, columns)
+    mz_cols <- intersect(columns, c("mz", "intensity"))
+    local_cols <- intersect(columns, colnames(x@localData))
     core_cols <- intersect(columns, names(Spectra:::.SPECTRA_DATA_COLUMNS))
-    core_cols <- core_cols[!core_cols %in% c(db_cols, "mz", "intensity")]
-    res <- data.frame()
+    core_cols <- core_cols[!core_cols %in% c(db_cols, mz_cols, local_cols)]
+    res <- NULL
+    ## Get data from database
     if (length(db_cols)) {
-        qry <- dbSendQuery(
-            x@dbcon, paste0("select ", paste(db_cols, collapse = ","),
-                            " from spectra_data where spectrum_id = ?"))
-        qry <- dbBind(qry, list(x@spectraIds))
-        res <- dbFetch(qry)
-        dbClearResult(qry)
+        res <- DataFrame(.fetch_spectra_data_sql(x, columns = db_cols))
     }
-    if (any(columns %in% c("mz", "intensity"))) {
+    ## Get m/z and intensity values
+    if (length(mz_cols)) {
+        pks <- .fetch_peaks_sql(x, columns = mz_cols)
+        f <- factor(pks$spectrum_id, levels = x@spectraIds)
+        if (any(mz_cols == "mz")) {
+            if (length(res))
+                res$mz <- NumericList(split(pks$mz, f), compress = FALSE)
+            else res <- DataFrame(mz = NumericList(split(pks$mz, f),
+                                                   compress = FALSE))
+        }
+        if (any(mz_cols == "intensity")) {
+            if (length(res))
+                res$intensity <- NumericList(split(pks$intensity, f),
+                                             compress = FALSE)
+            else res <- DataFrame(intensity = NumericList(
+                                      split(pks$intensity, f),
+                                      compress = FALSE))
+        }
     }
+    ## Get local data
+    ## Create missing core variables
     if (length(core_cols)) {
-        tmp <- as.data.frame(
-            lapply(Spectra:::.SPECTRA_DATA_COLUMNS[core_cols],
-                   function(z, n) rep(as(NA, z), n), n = length(x)))
-        if (nrow(res))
+        tmp <- DataFrame(lapply(Spectra:::.SPECTRA_DATA_COLUMNS[core_cols],
+                                function(z, n) rep(as(NA, z), n), length(x)))
+        if (length(res))
             res <- cbind(res, tmp)
         else res <- tmp
         if (any(core_cols == "dataStorage"))
             res$dataStorage <- dataStorage(x)
     }
+    if (!all(columns %in% colnames(res)))
+        stop("Column(s) ", paste0(columns[!columns %in% names(res)],
+                                  collapse = ", "), " not available.",
+             call. = FALSE)
     res[, columns, drop = FALSE]
+}
+
+.fetch_peaks_sql <- function(x, columns = c("mz", "intensity")) {
+    if (length(x@dbcon)) {
+        qry <- dbSendQuery(x@dbcon, paste0("select spectrum_id,",
+                                           paste(columns, collapse = ","),
+                                           " from peaks where spectrum_id = ?"))
+        qry <- dbBind(qry, list(x@spectraIds))
+        res <- dbFetch(qry)
+        dbClearResult(qry)
+        res
+    } else {
+        data.frame(spectrum_id = character(), mz = numeric(),
+                   intensity = numeric())
+    }
+}
+
+.fetch_spectra_data_sql <- function(x, columns = c("spectrum_id")) {
+    qry <- dbSendQuery(
+        x@dbcon, paste0("select ", paste(columns, collapse = ","),
+                        " from spectra_data where spectrum_id = ?"))
+    qry <- dbBind(qry, list(x@spectraIds))
+    res <- dbFetch(qry)
+    dbClearResult(qry)
+    if (any(columns == "msLevel"))
+        res$msLevel <- as.integer(sub("MS", "", res$msLevel))
+    if (any(columns == "polarity")) {
+        pol <- rep(NA_integer_, nrow(res))
+        pol[res$polarity == "POSITIVE"] <- 1L
+        pol[res$polarity == "NEGATIVE"] <- 0L
+        res$polarity <- pol
+    }
+    res
 }
--- tests/testthat/test_MsBackendMassbankDb-functions.R
+++ tests/testthat/test_MsBackendMassbankDb-functions.R
@@ -14,3 +14,91 @@ test_that(".spectra_data_massbank works", {
 
     res <- .spectra_data_massbank(be)
 })
+
+test_that(".fetch_peaks_sql works", {
+    be <- MsBackendMassbankDb()
+    res <- .fetch_peaks_sql(be)
+    expect_true(is.data.frame(res))
+    expect_equal(colnames(res), c("spectrum_id", "mz", "intensity"))
+    expect_true(nrow(res) == 0)
+
+    be <- backendInitialize(MsBackendMassbankDb(), dbc)
+    res <- .fetch_peaks_sql(be)
+    expect_true(is.data.frame(res))
+    expect_equal(colnames(res), c("spectrum_id", "mz", "intensity"))
+    expect_true(nrow(res) > 0)
+})
+
+test_that(".fetch_spectra_data_sql works", {
+    be <- backendInitialize(MsBackendMassbankDb(), dbc)
+    res <- .fetch_spectra_data_sql(be)
+    expect_true(nrow(res) > 0)
+    expect_true(colnames(res) == "spectrum_id")
+
+    res <- .fetch_spectra_data_sql(be, columns = "msLevel")
+    expect_true(all(res$msLevel == 2L))
+
+    res <- .fetch_spectra_data_sql(be, columns = "polarity")
+    expect_true(all(res$polarity == 1L))
+})
+
+test_that(".spectra_data_massbank_sql works", {
+    be <- MsBackendMassbankDb()
+    res <- .spectra_data_massbank_sql(be)
+    expect_true(is(res, "DataFrame"))
+    expect_true(nrow(res) == 0L)
+    expect_equal(colnames(res), names(Spectra:::.SPECTRA_DATA_COLUMNS))
+
+    be <- backendInitialize(MsBackendMassbankDb(), dbc)
+    ## Full data.
+    expect_error(.spectra_data_massbank_sql(be, columns = "other"), "available")
+    res <- .spectra_data_massbank_sql(be)
+    expect_true(is(res, "DataFrame"))
+    expect_equal(colnames(res), spectraVariables(be))
+    expect_true(is(res$mz, "NumericList"))
+    expect_true(is(res$intensity, "NumericList"))
+    expect_true(all(is.na(res$rtime)))
+
+    ## A single column
+    res <- .spectra_data_massbank_sql(be, columns = "rtime")
+    expect_true(is(res, "DataFrame"))
+    expect_true(nrow(res) > 0)
+    expect_equal(colnames(res), "rtime")
+    expect_true(all(is.na(res$rtime)))
+
+    res <- .spectra_data_massbank_sql(be, "msLevel")
+    expect_true(is(res, "DataFrame"))
+    expect_true(nrow(res) > 0)
+    expect_equal(colnames(res), "msLevel")
+    expect_true(all(res$msLevel == 2L))
+
+    res <- .spectra_data_massbank_sql(be, "mz")
+    expect_true(is(res, "DataFrame"))
+    expect_true(nrow(res) > 0)
+    expect_equal(colnames(res), "mz")
+    expect_true(is(res$mz, "NumericList"))
+
+    ## A combination of core and db
+    res <- .spectra_data_massbank_sql(be, columns = c("rtime", "polarity"))
+    expect_true(is(res, "DataFrame"))
+    expect_true(nrow(res) > 0)
+    expect_equal(colnames(res), c("rtime", "polarity"))
+    expect_true(all(res$polarity == 1L))
+    expect_true(all(is.na(res$rtime)))
+
+    res <- .spectra_data_massbank_sql(
+        be, columns = c("mz", "polarity", "intensity"))
+    expect_true(is(res, "DataFrame"))
+    expect_true(nrow(res) > 0)
+    expect_equal(colnames(res), c("mz", "polarity", "intensity"))
+    expect_true(all(res$polarity == 1L))
+    expect_true(is(res$mz, "NumericList"))
+    expect_true(is(res$intensity, "NumericList"))
+
+    ## With local data.
+    stop("implement local data")
+    ## - validation method that checks that nrow @localData and @spectrumIds
+    ##   have same length.
+    ## - spectraVariables adds also columns from there (unique one).
+    ## - spectraData takes columns FIRST from there (not from other sources).
+)
